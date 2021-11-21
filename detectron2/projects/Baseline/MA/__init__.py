4286497425d283f931532a05663f86cd13fb1c9f
Yu Cao <cyzhoumoon@gmail.com>
2021-11-21 08:45:33 +0100

    implement

---
+++ detectron2/projects/Baseline/MA/__init__.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""
+@Project ：Masterarbeit
+@File    ：__init__.py.py
+@Author  ：Yu Cao
+@Date    ：2021/11/21 8:43 
+"""
+
+from .config import add_panoptic_deeplab_config
+from .dataset_mapper import PanopticDeeplabDatasetMapper
+from .panoptic_seg import (
+    PanopticDeepLab,
+    INS_EMBED_BRANCHES_REGISTRY,
+    build_ins_embed_branch,
+    PanopticDeepLabSemSegHead,
+    PanopticDeepLabInsEmbedHead,
+)
