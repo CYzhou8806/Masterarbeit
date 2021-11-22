6ac6a4acc2335a69a96377fab7148120ebadb9e4
Yu Cao <cyzhoumoon@gmail.com>
2021-11-22 10:11:08 +0100

    implement

--- detectron2/projects/Baseline/MA/network.py
+++ detectron2/projects/Baseline/MA/network.py
@@ -107,8 +107,9 @@ class JointEstimation(nn.Module):
         images = ImageList.from_tensors(images, size_divisibility)
 
         features_L = self.backbone(images.tensor)
-
         losses = {}
+
+        # semantic branch
         if "sem_seg" in batched_inputs[0]:
             targets = [x["sem_seg"].to(self.device) for x in batched_inputs]
             targets = ImageList.from_tensors(
@@ -124,9 +125,10 @@ class JointEstimation(nn.Module):
         else:
             targets = None
             weights = None
-        sem_seg_results, sem_seg_losses = self.sem_seg_head(features, targets, weights)
+        sem_seg_results, sem_seg_losses, sem_seg_features = self.sem_seg_head(features, targets, weights)
         losses.update(sem_seg_losses)
 
+        # instance branch
         if "center" in batched_inputs[0] and "offset" in batched_inputs[0]:
             center_targets = [x["center"].to(self.device) for x in batched_inputs]
             center_targets = ImageList.from_tensors(
@@ -134,7 +136,6 @@ class JointEstimation(nn.Module):
             ).tensor.unsqueeze(1)
             center_weights = [x["center_weights"].to(self.device) for x in batched_inputs]
             center_weights = ImageList.from_tensors(center_weights, size_divisibility).tensor
-
             offset_targets = [x["offset"].to(self.device) for x in batched_inputs]
             offset_targets = ImageList.from_tensors(offset_targets, size_divisibility).tensor
             offset_weights = [x["offset_weights"].to(self.device) for x in batched_inputs]
@@ -142,11 +143,9 @@ class JointEstimation(nn.Module):
         else:
             center_targets = None
             center_weights = None
-
             offset_targets = None
             offset_weights = None
-
-        center_results, offset_results, center_losses, offset_losses = self.ins_embed_head(
+        center_results, offset_results, center_losses, offset_losses, ins_seg_features = self.ins_embed_head(
             features, center_targets, center_weights, offset_targets, offset_weights
         )
         losses.update(center_losses)
@@ -393,3 +392,238 @@ def build_ins_embed_branch(cfg, input_shape):
     """
     name = cfg.MODEL.INS_EMBED_HEAD.NAME
     return INS_EMBED_BRANCHES_REGISTRY.get(name)(cfg, input_shape)
+
+
+@INS_EMBED_BRANCHES_REGISTRY.register()
+class JointEstimationInsEmbedHead(DeepLabV3PlusHead):
+    """
+    A instance embedding head described in :paper:`Panoptic-DeepLab`.
+    """
+
+    @configurable
+    def __init__(
+            self,
+            input_shape: Dict[str, ShapeSpec],
+            *,
+            decoder_channels: List[int],
+            norm: Union[str, Callable],
+            head_channels: int,
+            center_loss_weight: float,
+            offset_loss_weight: float,
+            **kwargs,
+    ):
+        """
+        NOTE: this interface is experimental.
+
+        Args:
+            input_shape (ShapeSpec): shape of the input feature
+            decoder_channels (list[int]): a list of output channels of each
+                decoder stage. It should have the same length as "input_shape"
+                (each element in "input_shape" corresponds to one decoder stage).
+            norm (str or callable): normalization for all conv layers.
+            head_channels (int): the output channels of extra convolutions
+                between decoder and predictor.
+            center_loss_weight (float): loss weight for center point prediction.
+            offset_loss_weight (float): loss weight for center offset prediction.
+        """
+        super().__init__(input_shape, decoder_channels=decoder_channels, norm=norm, **kwargs)
+        assert self.decoder_only
+
+        self.center_loss_weight = center_loss_weight
+        self.offset_loss_weight = offset_loss_weight
+        use_bias = norm == ""
+        # center prediction
+        # `head` is additional transform before predictor
+        self.center_head = nn.Sequential(
+            Conv2d(
+                decoder_channels[0],
+                decoder_channels[0],
+                kernel_size=3,
+                padding=1,
+                bias=use_bias,
+                norm=get_norm(norm, decoder_channels[0]),
+                activation=F.relu,
+            ),
+            Conv2d(
+                decoder_channels[0],
+                head_channels,
+                kernel_size=3,
+                padding=1,
+                bias=use_bias,
+                norm=get_norm(norm, head_channels),
+                activation=F.relu,
+            ),
+        )
+        weight_init.c2_xavier_fill(self.center_head[0])
+        weight_init.c2_xavier_fill(self.center_head[1])
+        self.center_predictor = Conv2d(head_channels, 1, kernel_size=1)
+        nn.init.normal_(self.center_predictor.weight, 0, 0.001)
+        nn.init.constant_(self.center_predictor.bias, 0)
+
+        # offset prediction
+        # `head` is additional transform before predictor
+        if self.use_depthwise_separable_conv:
+            # We use a single 5x5 DepthwiseSeparableConv2d to replace
+            # 2 3x3 Conv2d since they have the same receptive field.
+            self.offset_head = DepthwiseSeparableConv2d(
+                decoder_channels[0],
+                head_channels,
+                kernel_size=5,
+                padding=2,
+                norm1=norm,
+                activation1=F.relu,
+                norm2=norm,
+                activation2=F.relu,
+            )
+        else:
+            self.offset_head = nn.Sequential(
+                Conv2d(
+                    decoder_channels[0],
+                    decoder_channels[0],
+                    kernel_size=3,
+                    padding=1,
+                    bias=use_bias,
+                    norm=get_norm(norm, decoder_channels[0]),
+                    activation=F.relu,
+                ),
+                Conv2d(
+                    decoder_channels[0],
+                    head_channels,
+                    kernel_size=3,
+                    padding=1,
+                    bias=use_bias,
+                    norm=get_norm(norm, head_channels),
+                    activation=F.relu,
+                ),
+            )
+            weight_init.c2_xavier_fill(self.offset_head[0])
+            weight_init.c2_xavier_fill(self.offset_head[1])
+        self.offset_predictor = Conv2d(head_channels, 2, kernel_size=1)
+        nn.init.normal_(self.offset_predictor.weight, 0, 0.001)
+        nn.init.constant_(self.offset_predictor.bias, 0)
+
+        self.center_loss = nn.MSELoss(reduction="none")
+        self.offset_loss = nn.L1Loss(reduction="none")
+
+    @classmethod
+    def from_config(cls, cfg, input_shape):
+        if cfg.INPUT.CROP.ENABLED:
+            assert cfg.INPUT.CROP.TYPE == "absolute"
+            train_size = cfg.INPUT.CROP.SIZE
+        else:
+            train_size = None
+        decoder_channels = [cfg.MODEL.INS_EMBED_HEAD.CONVS_DIM] * (
+                len(cfg.MODEL.INS_EMBED_HEAD.IN_FEATURES) - 1
+        ) + [cfg.MODEL.INS_EMBED_HEAD.ASPP_CHANNELS]
+        ret = dict(
+            input_shape={
+                k: v for k, v in input_shape.items() if k in cfg.MODEL.INS_EMBED_HEAD.IN_FEATURES
+            },
+            project_channels=cfg.MODEL.INS_EMBED_HEAD.PROJECT_CHANNELS,
+            aspp_dilations=cfg.MODEL.INS_EMBED_HEAD.ASPP_DILATIONS,
+            aspp_dropout=cfg.MODEL.INS_EMBED_HEAD.ASPP_DROPOUT,
+            decoder_channels=decoder_channels,
+            common_stride=cfg.MODEL.INS_EMBED_HEAD.COMMON_STRIDE,
+            norm=cfg.MODEL.INS_EMBED_HEAD.NORM,
+            train_size=train_size,
+            head_channels=cfg.MODEL.INS_EMBED_HEAD.HEAD_CHANNELS,
+            center_loss_weight=cfg.MODEL.INS_EMBED_HEAD.CENTER_LOSS_WEIGHT,
+            offset_loss_weight=cfg.MODEL.INS_EMBED_HEAD.OFFSET_LOSS_WEIGHT,
+            use_depthwise_separable_conv=cfg.MODEL.SEM_SEG_HEAD.USE_DEPTHWISE_SEPARABLE_CONV,
+        )
+        return ret
+
+    def forward(
+            self,
+            features,
+            center_targets=None,
+            center_weights=None,
+            offset_targets=None,
+            offset_weights=None,
+    ):
+        """
+        Returns:
+            In training, returns (None, dict of losses)
+            In inference, returns (CxHxW logits, {})
+        """
+        center, offset, out_features = self.layers(features)
+        if self.training:
+            return (
+                None,
+                None,
+                self.center_losses(center, center_targets, center_weights),
+                self.offset_losses(offset, offset_targets, offset_weights),
+                out_features,
+            )
+        else:
+            center = F.interpolate(
+                center, scale_factor=self.common_stride, mode="bilinear", align_corners=False
+            )
+            offset = (
+                    F.interpolate(
+                        offset, scale_factor=self.common_stride, mode="bilinear", align_corners=False
+                    )
+                    * self.common_stride
+            )
+            return center, offset, {}, {}, out_features
+
+    def layers(self, features):
+        assert self.decoder_only
+        out_features = {}
+        # Reverse feature maps into top-down order (from low to high resolution)
+        for i, f in enumerate(self.in_features[::-1]):
+            x = features[f]  # "features" is dictionary
+            proj_x = self.decoder[f]["project_conv"](x)
+            if self.decoder[f]["fuse_conv"] is None:
+                # This is aspp module
+                y = proj_x
+            else:
+                # Upsample y
+                y = F.interpolate(y, size=proj_x.size()[2:], mode="bilinear", align_corners=False)
+                y = torch.cat([proj_x, y], dim=1)
+                y = self.decoder[f]["fuse_conv"](y)
+
+            # save outputs
+            if i == 1:
+                out_features['1/8'] = y
+            elif i == 2:
+                out_features['1/4'] = y
+            elif i == 0:
+                out_features['1/16'] = y
+            else:
+                raise ValueError("undefined output of SemSeg Branch")
+        y = out_features['1/4']
+        # center
+        center = self.center_head(y)
+        center = self.center_predictor(center)
+        # offset
+        offset = self.offset_head(y)
+        offset = self.offset_predictor(offset)
+        return center, offset, out_features
+
+    def center_losses(self, predictions, targets, weights):
+        predictions = F.interpolate(
+            predictions, scale_factor=self.common_stride, mode="bilinear", align_corners=False
+        )
+        loss = self.center_loss(predictions, targets) * weights
+        if weights.sum() > 0:
+            loss = loss.sum() / weights.sum()
+        else:
+            loss = loss.sum() * 0
+        losses = {"loss_center": loss * self.center_loss_weight}
+        return losses
+
+    def offset_losses(self, predictions, targets, weights):
+        predictions = (
+                F.interpolate(
+                    predictions, scale_factor=self.common_stride, mode="bilinear", align_corners=False
+                )
+                * self.common_stride
+        )
+        loss = self.offset_loss(predictions, targets) * weights
+        if weights.sum() > 0:
+            loss = loss.sum() / weights.sum()
+        else:
+            loss = loss.sum() * 0
+        losses = {"loss_offset": loss * self.offset_loss_weight}
+        return losses
