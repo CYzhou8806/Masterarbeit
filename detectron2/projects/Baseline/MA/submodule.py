759b6ff01d6fa6a7940979266a6092c722f5faf7
Yu Cao <cyzhoumoon@gmail.com>
2021-11-29 11:55:14 +0100

    debug

--- detectron2/projects/Baseline/MA/submodule.py
+++ detectron2/projects/Baseline/MA/submodule.py
@@ -12,7 +12,7 @@ from detectron2.layers.batch_norm import FrozenBatchNorm2d, get_norm
 def convbn(in_planes, out_planes, kernel_size, stride, pad, dilation):
     return nn.Sequential(nn.Conv2d(in_planes, out_planes, kernel_size=kernel_size, stride=stride,
                                    padding=dilation if dilation > 1 else pad, dilation=dilation, bias=False),
-                         nn.BatchNorm2d(out_planes))
+                         nn.BatchNorm2d(out_planes)).cuda()
 
 
 '''
