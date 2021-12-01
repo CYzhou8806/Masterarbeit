b0ec2ef3ff984bb1356906db6940493cbaa1ae33
Yu Cao <cyzhoumoon@gmail.com>
2021-12-01 19:55:33 +0100

    correct network

--- detectron2/projects/Baseline/MA/tools.py
+++ detectron2/projects/Baseline/MA/tools.py
@@ -7,22 +7,33 @@
 @Date    ：2021/12/1 19:19 
 """
 import os
+import shutil
 
 import cv2
 
 
-def down_samples_dataset(dataset_root, output_root, scale):
+def down_samples_dataset(dataset_root, output_root=None, scale=16):
     for root, dirs, files in os.walk(dataset_root):
         for file in files:
             if os.path.splitext(file)[-1] == ".png":
-                pass
+                file_path = os.path.join(root, file)
+                img = cv2.imread(img)
+                img_down = cv2.resize(img, (img.shape[0]//scale, img.shape[1]//scale), interpolation=cv2.INTER_NEAREST)
+                '''
+                cv2.imshow('', img_down)
+                cv2.waitKey(0)
+                cv2.destroyAllWindows()
+                '''
+                save_path = file_path.replace("datasets", "dataset")
+                save_dir = save_path.split('/')[0]
+                if not os.path.exists(save_dir):
+                    os.makedirs(save_dir)
+                cv2.imwrite(save_path, img_down)
+            else:
+                file_path = os.path.join(root, file)
+                shutil.copytree(file_path, file_path.replace("datasets", "dataset"))
 
 
-img =
-pic = cv2.imread(img)
-
-'''
-pic_n = cv2.resize(pic, (1280, 720))
-pic_name = i
-cv2.imwrite(os.path.join(resultDir, i), pic_n)
-'''
+if __name__ == "__main__":
+    input_root = "/home/eistrauben/github/Masterarbeit/detectron2/projects/Baseline/datasets/cityscapes"
+    down_samples_dataset(input_root, scale=16)
