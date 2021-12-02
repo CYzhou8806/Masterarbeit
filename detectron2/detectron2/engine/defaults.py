3291b099c194062f94f49f3fe7c5ed8a759c3b1a
Yu Cao <cyzhoumoon@gmail.com>
2021-12-02 20:51:59 +0100

    debug

--- detectron2/detectron2/engine/defaults.py
+++ detectron2/detectron2/engine/defaults.py
@@ -374,6 +374,8 @@ class DefaultTrainer(TrainerBase):
 
         # Assume these objects must be constructed in this order.
         model = self.build_model(cfg)
+        model.state_dict()
+
         optimizer = self.build_optimizer(cfg, model)
         data_loader = self.build_train_loader(cfg)
 
