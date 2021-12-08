a260e0d97e2f4e29cfe6c8b8b2f7207faa46b037
Yu Cao <cyzhoumoon@gmail.com>
2021-12-08 15:21:06 +0100

    debug

--- detectron2/projects/Baseline/PD_demo.py
+++ detectron2/projects/Baseline/PD_demo.py
@@ -66,7 +66,7 @@ def get_parser():
     parser.add_argument(
         "--opts",
         help="Modify config options using the command-line 'KEY VALUE' pairs",
-        default=['MODEL.WEIGHTS', 'model/init_panoptic_cityscapes_weights.pth'],
+        default=['MODEL.WEIGHTS', 'model/model_0109999.pth'],
         nargs=argparse.REMAINDER,
     )
     return parser
