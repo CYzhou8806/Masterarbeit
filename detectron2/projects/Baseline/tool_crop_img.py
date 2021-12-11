d4d0267fbe9246ad4a73b0925ee1a6f9caaf18a1
Yu Cao <cyzhoumoon@gmail.com>
2021-12-11 12:46:54 +0100

    debug

---
+++ detectron2/projects/Baseline/tool_crop_img.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""
+@Project ：Masterarbeit
+@File    ：tool_crop_img.py
+@Author  ：Yu Cao
+@Date    ：2021/12/11 12:42 
+"""
+
+from PIL import Image
+
+path_left = "datasets/kitti_2015/data_scene_flow/training/image_2/000004_10.png"
+path_right = "datasets/kitti_2015/data_scene_flow/training/image_3/000004_10.png"
+
+
+img = Image.open(path_left)
+width, height = img.size
+# 前两个坐标点是左上角坐标
+# 后两个坐标点是右下角坐标
+# width在前， height在后
+box = (100, 20, 612, 276)
+region = img.crop(box)
+region.save(path_left.replace('_10', '_10_crop'))
+
+img = Image.open(path_right)
+region = img.crop(box)
+region.save(path_right.replace('_10', '_10_crop'))
