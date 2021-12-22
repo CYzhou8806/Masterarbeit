d5c989a55942d7934e47d382dc5088a448a90ef6
Yu Cao <cyzhoumoon@gmail.com>
2021-12-22 08:35:01 +0100

    debug

---
+++ detectron2/projects/Baseline/tool_del_prediction.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+# -*-coding:utf-8 -*-
+"""
+@Project ：Masterarbeit
+@File    ：tool_del_prediction.py
+@Author  ：Yu Cao
+@Date    ：2021/12/22 8:21 
+"""
+import os
+
+for root, dirs, files in os.walk(r'C:\Users\cyzho\Desktop\demo_test\prediction'):
+    for file in files:
+        if os.path.splitext(file)[0][-1] == '1':
+            os.remove(os.path.join(root, file))
