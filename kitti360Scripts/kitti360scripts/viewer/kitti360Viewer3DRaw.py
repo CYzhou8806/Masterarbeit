56207b9f795eac599697784ccc92ad09aeb7443d
Yu Cao <cyzhoumoon@gmail.com>
2021-12-27 13:20:05 +0100

    debug

--- kitti360Scripts/kitti360scripts/viewer/kitti360Viewer3DRaw.py
+++ kitti360Scripts/kitti360scripts/viewer/kitti360Viewer3DRaw.py
@@ -27,6 +27,7 @@ from tqdm import tqdm
 
 os.environ["KITTI360_DATASET"] = r"D:\Masterarbeit\dataset\kitti_360"
 
+
 # the main class that loads raw 3D scans
 class Kitti360Viewer3DRaw(object):
 
@@ -37,34 +38,34 @@ class Kitti360Viewer3DRaw(object):
             kitti360Path = os.environ['KITTI360_DATASET']
         else:
             kitti360Path = os.path.join(os.path.dirname(
-                                os.path.realpath(__file__)), '..', '..')
+                os.path.realpath(__file__)), '..', '..')
 
-        if mode=='velodyne':
-            self.sensor_dir='velodyne_points'
-        elif mode=='sick':
-            self.sensor_dir='sick_points'
+        if mode == 'velodyne':
+            self.sensor_dir = 'velodyne_points'
+        elif mode == 'sick':
+            self.sensor_dir = 'sick_points'
         else:
             raise RuntimeError('Unknown sensor type!')
 
         sequence = '2013_05_28_drive_%04d_sync' % seq
-        self.raw3DPcdPath  = os.path.join(kitti360Path, 'data_3d_raw', sequence, self.sensor_dir, 'data')
+        self.raw3DPcdPath = os.path.join(kitti360Path, 'data_3d_raw', sequence, self.sensor_dir, 'data')
 
     def loadVelodyneData(self, frame=0):
         pcdFile = os.path.join(self.raw3DPcdPath, '%010d.bin' % frame)
         if not os.path.isfile(pcdFile):
             raise RuntimeError('%s does not exist!' % pcdFile)
         pcd = np.fromfile(pcdFile, dtype=np.float32)
-        pcd = np.reshape(pcd,[-1,4])
-        return pcd 
+        pcd = np.reshape(pcd, [-1, 4])
+        return pcd
 
     def loadSickData(self, frame=0):
         pcdFile = os.path.join(self.raw3DPcdPath, '%010d.bin' % frame)
         if not os.path.isfile(pcdFile):
             raise RuntimeError('%s does not exist!' % pcdFile)
         pcd = np.fromfile(pcdFile, dtype=np.float32)
-        pcd = np.reshape(pcd,[-1,2])
-        pcd = np.concatenate([np.zeros_like(pcd[:,0:1]), -pcd[:,0:1], pcd[:,1:2]], axis=1)
-        return pcd 
+        pcd = np.reshape(pcd, [-1, 2])
+        pcd = np.concatenate([np.zeros_like(pcd[:, 0:1]), -pcd[:, 0:1], pcd[:, 1:2]], axis=1)
+        return pcd
 
 
 def projectVeloToImage(cam_id=0, seq=0):
@@ -77,9 +78,9 @@ def projectVeloToImage(cam_id=0, seq=0):
         kitti360Path = os.environ['KITTI360_DATASET']
     else:
         kitti360Path = os.path.join(os.path.dirname(
-                                os.path.realpath(__file__)), '..', '..')
+            os.path.realpath(__file__)), '..', '..')
 
-    sequence = '2013_05_28_drive_%04d_sync'%seq
+    sequence = '2013_05_28_drive_%04d_sync' % seq
 
     output_root = os.path.join(kitti360Path, "disparity")
     if not os.path.exists(output_root):
@@ -91,17 +92,17 @@ def projectVeloToImage(cam_id=0, seq=0):
         print("---  del old and create new folder...  ---")
 
     # perspective camera
-    if cam_id in [0,1]:
+    if cam_id in [0, 1]:
         camera = CameraPerspective(kitti360Path, sequence, cam_id)
     # fisheye camera
-    elif cam_id in [2,3]:
+    elif cam_id in [2, 3]:
         camera = CameraFisheye(kitti360Path, sequence, cam_id)
     else:
         raise RuntimeError('Unknown camera ID!')
 
     # object for parsing 3d raw data 
     velo = Kitti360Viewer3DRaw(mode='velodyne', seq=seq)
-    
+
     # cam_0 to velo
     fileCameraToVelo = os.path.join(kitti360Path, 'calibration', 'calib_cam_to_velo.txt')
     TrCam0ToVelo = loadCalibrationRigid(fileCameraToVelo)
@@ -118,9 +119,9 @@ def projectVeloToImage(cam_id=0, seq=0):
         TrCamToVelo = TrCam0ToVelo @ TrCamkToCam0
         # Tr(velo -> cam_k)
         TrVeloToCam[k] = np.linalg.inv(TrCamToVelo)
-    
+
     # take the rectification into account for perspective cameras
-    if cam_id==0 or cam_id == 1:
+    if cam_id == 0 or cam_id == 1:
         TrVeloToRect = np.matmul(camera.R_rect, TrVeloToCam['image_%02d' % cam_id])
     else:
         TrVeloToRect = TrVeloToCam['image_%02d' % cam_id]
@@ -132,26 +133,27 @@ def projectVeloToImage(cam_id=0, seq=0):
     # for each frame, load the raw 3D scan and project to image plane
     for frame in tqdm(range(0, 1000, 2)):
         points = velo.loadVelodyneData(frame)
-        points[:,3] = 1
+        points[:, 3] = 1
 
         # transfrom velodyne points to camera coordinate
         pointsCam = np.matmul(TrVeloToRect, points.T).T
-        pointsCam = pointsCam[:,:3]
+        pointsCam = pointsCam[:, :3]
         # project to image space
-        u,v, depth= camera.cam2image(pointsCam.T)
+        u, v, depth = camera.cam2image(pointsCam.T)
         u = u.astype(np.int)
         v = v.astype(np.int)
 
         # prepare depth map for visualization
         depthMap = np.zeros((camera.height, camera.width))
         depthImage = np.zeros((camera.height, camera.width, 3))
-        mask = np.logical_and(np.logical_and(np.logical_and(u>=0, u<camera.width), v>=0), v<camera.height)
+        mask = np.logical_and(np.logical_and(np.logical_and(u >= 0, u < camera.width), v >= 0), v < camera.height)
         # visualize points within 30 meters
-        mask = np.logical_and(np.logical_and(mask, depth>0), depth<99999)
-        depthMap[v[mask],u[mask]] = depth[mask]
+        mask = np.logical_and(np.logical_and(mask, depth > 0), depth < 99999)
+        depthMap[v[mask], u[mask]] = depth[mask]
 
         sub_dir = 'data_rect' if cam_id in [0, 1] else 'data_rgb'
-        imagePath = os.path.join(kitti360Path, 'data_2d_raw', sequence, 'image_%02d' % cam_id, sub_dir, '%010d.png' % frame)
+        imagePath = os.path.join(kitti360Path, 'data_2d_raw', sequence, 'image_%02d' % cam_id, sub_dir,
+                                 '%010d.png' % frame)
         if not os.path.isfile(imagePath):
             raise RuntimeError('Image file %s does not exist!' % imagePath)
         img_name = os.path.basename(imagePath)
@@ -184,7 +186,9 @@ def projectVeloToImage(cam_id=0, seq=0):
         plt.suptitle('Sequence %04d, Camera %02d, Frame %010d' % (seq, cam_id, frame))
         plt.show()
         '''
-if __name__=='__main__':
+
+
+if __name__ == '__main__':
 
     visualizeIn2D = True
     # sequence index
@@ -192,7 +196,7 @@ if __name__=='__main__':
     # set it to 0 or 1 for projection to perspective images
     #           2 or 3 for projecting to fisheye images
     cam_id = 0
-    
+
     # visualize raw 3D velodyne scans in 2D
     if visualizeIn2D:
         projectVeloToImage(seq=seq, cam_id=cam_id)
@@ -203,12 +207,11 @@ if __name__=='__main__':
         frame = 1000
 
         v = Kitti360Viewer3DRaw(mode=mode)
-        if mode=='velodyne':
+        if mode == 'velodyne':
             points = v.loadVelodyneData(frame)
-        elif mode=='sick':
+        elif mode == 'sick':
             points = v.loadSickData(frame)
         pcd = open3d.geometry.PointCloud()
-        pcd.points = open3d.utility.Vector3dVector(points[:,:3])
-        
-        open3d.visualization.draw_geometries([pcd])
+        pcd.points = open3d.utility.Vector3dVector(points[:, :3])
 
+        open3d.visualization.draw_geometries([pcd])
