de197bc88783f6d1a0e26f288d00f1bc7f79cae2
Yu Cao <cyzhoumoon@gmail.com>
2021-12-28 17:04:14 +0100

    debug

--- detectron2/projects/Baseline/tools/check_disparity_gt.py
+++ detectron2/projects/Baseline/tools/check_disparity_gt.py
@@ -13,11 +13,10 @@ import sys
 import cv2
 import os
 from PIL import Image
+from tqdm import tqdm
 
 
-
-
-def to_warped_image(img, disp, direction_str):
+def to_warped_image(img, disp, direction_str, right):
     if direction_str == 'r2l':
         direction = - 1
     elif direction_str == 'l2r':
@@ -32,40 +31,99 @@ def to_warped_image(img, disp, direction_str):
     if disp.ndim == 3:
         disp = np.squeeze(disp, axis=-1)
 
+    old_map_x = map_x
     map_x = map_x + disp * direction
+    mask = old_map_x == map_x
+    assert not np.all(mask)
 
     for j in range(map_y.shape[1]):
         map_y[:, j] = [y for y in range(map_y.shape[0])]
 
-    left_warped = cv2.remap(img, map_x, map_y, cv2.INTER_NEAREST)
+    left_warped = cv2.remap(img, map_x, map_y, cv2.INTER_LINEAR)
 
-    return left_warped
+    left_new = np.array(left_warped)
+    left_new[mask] = 0
+    cv2.imwrite("1.jpg", left_new)
+    left_points = cv2.imread("1.jpg")
+
+    right_array = np.array(right)
+    right_array[mask] = 0
+    cv2.imwrite("2.jpg", right_array)
+    right_points = cv2.imread("2.jpg")
+
+    print(np.sum(left_new) - np.sum(mask)*3)
+
+    return left_warped, left_points, right_points
 
 
-#def check_disparity_gt():
+# def check_disparity_gt():
 
 if __name__ == "__main__":
     left_path = r"C:\Users\cyzho\Desktop\to_check_disparity\left\2013_05_28_drive_0002_sync_0000004613_left.png"
     # left = Image.open(left_path)
     left_path = r"D:\Masterarbeit\dataset\sceneflow\driving\left\000001_left.png"
+    left_path = r"D:\Masterarbeit\dataset\kitti 2015\kitti_2015\data_scene_flow\training\image_2\000000_10.png"
     left = cv2.imread(left_path)
     # left = np.array(left)
 
     right_path = r"C:\Users\cyzho\Desktop\to_check_disparity\right\2013_05_28_drive_0002_sync_0000004613_right.png"
     # right = Image.open(right_path
     right_path = r"D:\Masterarbeit\dataset\sceneflow\driving\right\000001_right.png"
+    right_path = r"D:\Masterarbeit\dataset\kitti 2015\kitti_2015\data_scene_flow\training\image_3\000000_10.png"
     right = cv2.imread(right_path)
     # right = np.array(right)
 
     disp_path = r"C:\Users\cyzho\Desktop\to_check_disparity\disparity\2013_05_28_drive_0002_sync_0000004613_disparity.tiff"
     disp_path = r"D:\Masterarbeit\dataset\sceneflow\driving\disparity\000001_disparity.tiff"
+    disp_path = r"D:\Masterarbeit\dataset\kitti 2015\kitti_2015\data_scene_flow\training\disp_occ_0\000000_10.png"
+    #disp_path = r"C:\Users\cyzho\Desktop\result_dis.png"
+
     disp = Image.open(disp_path)
-    disp = np.array(disp)
+    disp = np.array(disp, dtype=np.float32)
+    disp = disp/256.0
 
     direction_str = 'l2r'
-    #warped_img = to_warped_image(left, disp, direction_str)
+    warped_img, warped_points, right_points = to_warped_image(left, disp, direction_str, right)
     direction_str = 'r2l'
-    warped_img = to_warped_image(right, disp, direction_str)
-    cv2.imshow("Image", warped_img)
+    # warped_img, warped_points, right_points = to_warped_image(right, disp, direction_str,left)
+
+    left_warped_points = np.array(warped_points)
+    right_points = np.array(right_points)
+
+
+    diff_positions = []
+    for c in [0, 1, 2]:
+        for x in tqdm(range(left_warped_points.shape[0])):
+            for y in tqdm(range(left_warped_points.shape[1])):
+                if left_warped_points[x, y, c] != right_points[x, y, c]:
+                    '''
+                    print(left_warped_points[x, y, c])
+                    print('\n')
+                    print(right_points[x, y, c])
+                    print('\n')
+                    print('\n')
+                    print('\n')
+                    '''
+                    if abs(left_warped_points[x, y, c] - right_points[x, y, c]) < 1:
+                        diff_positions.append([x, y])
+                else:
+                    diff_positions.append([x, y])
+
+    print(np.all(left_warped_points == right_points))
+    print(np.sum(left_warped_points != right_points))
+    print(np.max(abs(left_warped_points-right_points)))
+
+    for [x,y] in tqdm(diff_positions):
+        left_warped_points[x,y] = 0
+
+    cv2.imwrite("3.jpg", left_warped_points)
+    left_differ = cv2.imread("3.jpg")
+
+
+
+    print('stop')
+
+    cv2.imshow("Image", left_differ)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+
