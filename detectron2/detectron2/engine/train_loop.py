59cf3e64df2768b0c26ea64a6e747411e5ea3d8e
Yu Cao <cyzhoumoon@gmail.com>
2022-01-31 18:23:33 +0100

    add commentary

--- detectron2/detectron2/engine/train_loop.py
+++ detectron2/detectron2/engine/train_loop.py
@@ -167,6 +167,8 @@ class TrainerBase:
                     self.after_step()
                     self.save_best = False
 
+                    if (self.iter + 1) / inter_pre_epoche > 500:
+                        raise RuntimeError("maximal epoch reached.")
                 # self.iter == max_iter can be used by `after_train` to
                 # tell whether the training successfully finished or failed
                 # due to exceptions.
