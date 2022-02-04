f3446d4a029fe289388a0b42d0913f72e17dce4d
Yu Cao <cyzhoumoon@gmail.com>
2022-02-05 00:59:49 +0100

    debug

--- detectron2/detectron2/engine/train_loop.py
+++ detectron2/detectron2/engine/train_loop.py
@@ -143,7 +143,7 @@ class TrainerBase:
         self.max_iter = max_iter
 
         self.save_best = False
-        self.cur_best = 9999
+        self.cur_best = 20.966806411743164
         with EventStorage(start_iter) as self.storage:
             try:
                 self.before_train()
