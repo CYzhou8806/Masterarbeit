e81f9a3c40b94dbd429365902eb4b090753c7462
Yu Cao <cyzhoumoon@gmail.com>
2022-03-07 20:57:51 +0100

    debug

--- detectron2/projects/Baseline/MA/network.py
+++ detectron2/projects/Baseline/MA/network.py
@@ -1624,7 +1624,7 @@ class share_module_more(nn.Module):
                 nn.Conv2d(3, 32, kernel_size=(3, 3), stride=(1, 1), padding=1, dilation=(1, 1), bias=False).cuda(),
                 nn.ReLU(inplace=True),
                 nn.Conv2d(32, 1, kernel_size=(3, 3), stride=(1, 1), padding=1, dilation=(1, 1), bias=False).cuda(),
-                nn.ReLU(inplace=True))
+                nn.ReLU(inplace=True)).cuda()
             layers['convs' + str(i)] = tmp
             # exec('self.convs{} = {}'.format(i,tmp))
 
