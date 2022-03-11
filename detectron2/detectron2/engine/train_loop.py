07b32eb00e5db52c95649580db10d26c53887f6a
Yu Cao <cyzhoumoon@gmail.com>
2022-03-11 22:52:50 +0100

    debug

--- detectron2/detectron2/engine/train_loop.py
+++ detectron2/detectron2/engine/train_loop.py
@@ -168,7 +168,7 @@ class TrainerBase:
                             wait_count = 0
                         else:
                             wait_count += 1
-                            if wait_count >= 400:
+                            if wait_count >= 6:
                                 raise RuntimeError("early stopping.")
                     self.after_step()
                     self.save_best = False
